20e5aae6e09be5aa899d347417f668e92c03bfbf
grok23 <46896171+grok23@users.noreply.github.com>
2019-04-28 19:59:38 +0100

    Blynk testing
    testing use of blynk to allow for remote control of vhicle using an android device over wifi

---
+++ Blynk_Serial_button/Blynk_Serial_button.ino
@@ -0,0 +1,60 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  This example shows how to use Arduino Yun Bridge
+  to connect your project to Blynk.
+
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <Bridge.h>
+#include <BlynkSimpleYun.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "1b0a4b5373644eb5b812400696d326dc";
+
+// enter the Wifi details in here. 
+char ssid[] = "eir48596114-2.4G";
+char pass[] = "turg2w5x";
+
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, IPAddress(192,168,1,100), 8080);
+}
+BLYNK_WRITE(V20) // V20 is the number of the button pin 
+{
+  int pinValue = param.asInt();
+  Serial.println(pinValue);
+}
+void loop()
+{
+  Blynk.run();
+}
---
+++ Blynk_serial/Blynk_serial.ino
@@ -0,0 +1,33 @@
+#include <Blynk.h>
+#define BLYNK_PRINT Serial
+#include <Bridge.h>
+#include <BlynkSimpleYun.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "1b0a4b5373644eb5b812400696d326dc";
+
+// enter the Wifi details in here. 
+char ssid[] = "eir48596114-2.4G";
+char pass[] = "turg2w5x";
+
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, (192,168,1,5), 8080);
+}
+BLYNK_WRITE(V6) // v6 is the virtual pin number.
+{
+  Serial.println(param.asStr());
+}
+
+void loop()
+{
+  Blynk.run();
+}
